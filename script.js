4ade85d26a553c405b5dd78d293546c1cef6517b
Sadek Mohammed <sadek.hmask@gmail.com>
2022-11-13 00:15:55 +0200

    adding math circle

--- script.js
+++ script.js
@@ -57,3 +57,24 @@ window.onload = function () {
     document.body.appendChild(css);
 };
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
